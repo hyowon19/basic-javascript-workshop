035b35a77261fcbdf8a9bf700c33df036e41a93b
Victor Wong <vj1901@hotmail.com>
2017-01-10 19:18:21 +0000

    added LastCharacter function

--- answers.js
+++ answers.js
@@ -1,6 +1,6 @@
-//This is the answers.js file for JavaScript Workshop from DecodeMTL
+// This is the answers.js file for JavaScript Workshop from DecodeMTL
 
-//This function will return the first character of a string.
+// This function will return the first character of a string.
 var FirstCharacter = function(someString) {
     return someString[0];
 }
@@ -8,3 +8,12 @@ var FirstCharacter = function(someString) {
 console.log(FirstCharacter("Hello World"));
 console.log(FirstCharacter("For Caesar!"));
 console.log(FirstCharacter(""));
+
+// This function will return the last character of a string.
+var LastCharacter = function(someString) {
+    return someString[(someString.length - 1)];
+}
+
+console.log(LastCharacter("Hello World"));
+console.log(LastCharacter("For Caesar!"));
+console.log(LastCharacter(""));
