cd972a8ccfa266168c8cb496086d0dbd35135e20
Victor Wong <vj1901@hotmail.com>
2017-01-10 19:30:29 +0000

    added characterPos function

--- answers.js
+++ answers.js
@@ -1,19 +1,28 @@
 // This is the answers.js file for JavaScript Workshop from DecodeMTL
 
 // This function will return the first character of a string.
-var FirstCharacter = function(someString) {
+var firstCharacter = function(someString) {
     return someString[0];
 }
 
-console.log(FirstCharacter("Hello World"));
-console.log(FirstCharacter("For Caesar!"));
-console.log(FirstCharacter(""));
+console.log(firstCharacter("Hello World"));
+console.log(firstCharacter("For Caesar!"));
+console.log(firstCharacter(""));
 
 // This function will return the last character of a string.
-var LastCharacter = function(someString) {
+var lastCharacter = function(someString) {
     return someString[(someString.length - 1)];
 }
 
-console.log(LastCharacter("Hello World"));
-console.log(LastCharacter("For Caesar!"));
-console.log(LastCharacter(""));
+console.log(lastCharacter("Hello World"));
+console.log(lastCharacter("For Caesar!"));
+console.log(lastCharacter(""));
+
+// This function will take a string and a number, and returns the character at the position represented by the number.
+var characterPos = function(someString, someNum) {
+  return someString[(someNum - 1)];  
+};
+
+console.log(characterPos("Heuioo there", 4));
+console.log(characterPos("Yippee Quick", 8));
+console.log(characterPos("", 3));
