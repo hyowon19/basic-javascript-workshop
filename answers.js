59454ba460340a3d542457bf5995b46c046c61e0
Victor Wong <vj1901@hotmail.com>
2017-01-10 19:10:31 +0000

    added FirstCharacter function

--- answers.js
+++ answers.js
@@ -1,1 +1,10 @@
-//This is a comment for this file.
+//This is the answers.js file for JavaScript Workshop from DecodeMTL
+
+//This function will return the first character of a string.
+var FirstCharacter = function(someString) {
+    return someString[0];
+}
+
+console.log(FirstCharacter("Hello World"));
+console.log(FirstCharacter("For Caesar!"));
+console.log(FirstCharacter(""));
